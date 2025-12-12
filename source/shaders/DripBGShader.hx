e01ed5519db3a18c64efb7cfbfb73ad9eda156f3
Cream.BR <creambr6@gmail.com>
2025-12-11 21:38:00 -0300

    Update DripBGShader.hx

--- source/shaders/DripBGShader.hx
+++ source/shaders/DripBGShader.hx
@@ -25,9 +25,6 @@ class DripBGShaderGLSL extends FlxShader{
     @:glFragmentSource('
         //SHADERTOY PORT FIX
         #pragma header
-        vec2 uv = openfl_TextureCoordv.xy;
-        vec2 fragCoord = openfl_TextureCoordv*openfl_TextureSize;
-        vec2 iResolution = openfl_TextureSize;
         uniform float iTime;
         #define iChannel0 bitmap
         #define texture flixel_texture2D
@@ -119,6 +116,8 @@ class DripBGShaderGLSL extends FlxShader{
         
         void main()
         {
+            vec2 fragCoord = openfl_TextureCoordv*openfl_TextureSize;
+            vec2 iResolution = openfl_TextureSize;
             vec2 uv = fragCoord.xy / iResolution.xy * 2. - 1.;
             uv.x *= iResolution.x/iResolution.y;
             
