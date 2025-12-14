391b0645097b02c3440920865bc7379b732c7c20
Cream.BR <creambr6@gmail.com>
2025-12-14 18:13:02 -0300

    Update ModifierSelectSubState.hx

--- source/substates/story/ModifierSelectSubState.hx
+++ source/substates/story/ModifierSelectSubState.hx
@@ -116,6 +116,11 @@ class ModifierSelectSubState extends StoryModeSubState
         });
         menu.add(filters);
 
+        #if mobile
+		addVirtualPad(UP_DOWN, NONE);
+		addVirtualPadCamera();
+		#end
+
         startGaming();
         changeSelection(0, false);
         changeSelection(-1, false);
